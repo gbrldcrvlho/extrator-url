a5dbadd5dbf3265eb21a95a38ce03ed9cba4ce81
gbrldcrvlho <gabrieldevcarvalho@proton.me>
2023-06-20 19:40:21 -0300

    primeira-aula-extraindo-informacoes

---
+++ main.py
@@ -0,0 +1,20 @@
+url = "https://bytebank.com/cambio?quantidade=100&moedaOrigem=real&moedaDestino=dolar"
+print(url)
+
+# Separa base e os parametros
+indice_interrogacao = url.find('?')
+url_base = url[0:indice_interrogacao]
+print(url_base)
+url_parametros = url[indice_interrogacao+1:]
+print(url_parametros)
+
+# Busca o valor de um parametro
+parametro_busca = 'moedaDestino'
+indice_parametro = url_parametros.find(parametro_busca)
+indice_valor = indice_parametro + len(parametro_busca) + 1
+indice_e_comercial = url_parametros.find('&', indice_valor)
+if indice_e_comercial == -1:
+    valor = url_parametros[indice_valor:]
+else:
+    valor = url_parametros[indice_valor:indice_e_comercial]
+print(valor)
