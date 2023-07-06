f0e9d7d1bdfd39dfaa3c5013fce683cd413cb0d5
gbrldcrvlho <gabrieldevcarvalho@proton.me>
2023-07-06 12:54:16 -0300

    strings-em-python-completo

--- extrator_url.py
+++ extrator_url.py
@@ -1,14 +1,24 @@
+import re
+
 class ExtratorURL:
     def __init__(self, url):
         self.url = self.sanitiza_url(url)
         self.valida_url()
 
     def sanitiza_url(self, url):
-        return url.strip()
+        if type(url) == str:
+            return url.strip()
+        else:
+            return ""
 
     def valida_url(self):
-        if self.url == "":
+        if not self.url:
             raise ValueError("A URL esta vazia")
+
+        padrao_url = re.compile('(http(s)?://)?(www.)?bytebank.com(.br)?/cambio')
+        match = padrao_url.match(self.url)
+        if not match:
+            raise ValueError("A URL nao e valida.")
 
     def get_url_base(self):
         indice_interrogacao = self.url.find('?')
@@ -17,7 +27,7 @@ class ExtratorURL:
 
     def get_url_parametros(self):
         indice_interrogacao = self.url.find('?')
-        url_parametros = self.url[indice_interrogacao: +1:]
+        url_parametros = self.url[indice_interrogacao +1:]
         return url_parametros
 
     def get_valor_parametro(self, parametro_busca):
@@ -30,7 +40,30 @@ class ExtratorURL:
             valor = self.get_url_parametros()[indice_valor:indice_e_comercial]
         return valor
 
+    def __len__(self):
+        return len(self.url)
 
-extrator_url = ExtratorURL("bytebank.com/cambio?quantidade=100&moedaOrigem=real&moedaDestino=dolar")
-valor_quantidade = extrator_url.get_valor_parametro("quantidade")
-print(valor_quantidade)
+    def __str__(self):
+        return self.url + "/n" + "Parametros: " + self.get_url_parametros() + "/n" + "URL Base: " + self.get_url_base()
+
+    def __eq__(self, other):
+        return self.url == other.url
+
+
+url = "bytebank.com/cambio?quantidade=100&moedaOrigem=real&moedaDestino=dolar"
+extrator_url = ExtratorURL(url)
+
+# Conversao de dolar para real
+VALOR_DOLAR = 5.50 # 1 dolar = 5.50 reais
+moeda_origem = extrator_url.get_valor_parametro("moedaOrigem")
+moeda_destino = extrator_url.get_valor_parametro("moedaDestino")
+quantidade = extrator_url.get_valor_parametro("quantidade")
+
+if moeda_origem == "real" and moeda_destino == "dolar":
+    valor_conversao = int(quantidade) / VALOR_DOLAR
+    print(f"O valor de R$ {quantidade} reais e igual a $ {valor_conversao:.2f} dolares.")
+elif moeda_origem == "dolar" and moeda_destino == "real":
+    valor_conversao = int(quantidade) * VALOR_DOLAR
+    print(f"O valor de $ {quantidade} dolares e igual a R$ {valor_conversao:.2f} reais.")
+else:
+    print(f"Cambio de {moeda_origem} para {moeda_destino} nao esta disponivel.")
